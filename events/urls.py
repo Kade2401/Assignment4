722931aad2cfd81a0beee11cda212402a5c0a829
Momongo <119149437+arystans@users.noreply.github.com>
2026-02-02 19:21:01 +0500

    yayaa

--- events/urls.py
+++ events/urls.py
@@ -2,7 +2,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.event_list, name='event_list'),
+    path('', views.register, name='register'),
+    path('events/', views.event_list, name='event_list'),
     path('create/', views.create_event, name='create_event'),
     path('<int:event_id>/', views.event_detail, name='event_detail'),
     path('<int:event_id>/edit/', views.edit_event, name='edit_event'),
