d7441704e9d6540434a0d064ba15a42a5f64968a
Yersultan_Muratbek <erlyk123@gmail.com>
2026-02-03 17:30:32 +0500

    """ Django settings for DjangoProject1 project.  Generated by 'django-admin startproject' using Django 6.0.  For more information on this file, see https://docs.djangoproject.com/en/6.0/topics/settings/  For the full list of settings and their values, see https://docs.djangoproject.com/en/6.0/ref/settings/ """  from pathlib import Path  # Build paths inside the project like this: BASE_DIR / 'subdir'. BASE_DIR = Path(__file__).resolve().parent.parent   # Quick-start development settings - unsuitable for production # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/  # SECURITY WARNING: keep the secret key used in production secret! SECRET_KEY = 'django-insecure-%3sa@l6iucafz(^z%@d*qw+o3*^v293z@%43dm^ltj^cul$_w$'  # SECURITY WARNING: don't run with debug turned on in production! DEBUG = False  LOGIN_URL = 'login' LOGIN_REDIRECT_URL = 'event_list' LOGOUT_REDIRECT_URL = 'login'   ALLOWED_HOSTS = ['*']   # Application definition  INSTALLED_APPS = [     'django.contrib.admin',     'django.contrib.auth',     'django.contrib.contenttypes',     'django.contrib.sessions',     'django.contrib.messages',     'django.contrib.staticfiles',     'events', ]  MIDDLEWARE = [     'django.middleware.security.SecurityMiddleware',     'whitenoise.middleware.WhiteNoiseMiddleware',     'django.contrib.sessions.middleware.SessionMiddleware',     'django.middleware.common.CommonMiddleware',     'django.middleware.csrf.CsrfViewMiddleware',     'django.contrib.sessions.middleware.SessionMiddleware',     'django.contrib.auth.middleware.AuthenticationMiddleware',     'django.contrib.messages.middleware.MessageMiddleware',     'django.middleware.clickjacking.XFrameOptionsMiddleware', ]  ROOT_URLCONF = 'DjangoProject1.urls'  TEMPLATES = [     {         'BACKEND': 'django.template.backends.django.DjangoTemplates',         'DIRS': [BASE_DIR / 'templates'],         'APP_DIRS': True,         'OPTIONS': {             'context_processors': [                 'django.template.context_processors.debug',                 'django.template.context_processors.request',                 'django.contrib.auth.context_processors.auth',                 'django.contrib.messages.context_processors.messages',             ],         },     }, ]  WSGI_APPLICATION = 'DjangoProject1.wsgi.application'   # Database # https://docs.djangoproject.com/en/6.0/ref/settings/#databases  DATABASES = {     'default': {         'ENGINE': 'django.db.backends.sqlite3',         'NAME': BASE_DIR / 'db.sqlite3',     } }   # Password validation # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators  AUTH_PASSWORD_VALIDATORS = [     {         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',     },     {         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',     },     {         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',     },     {         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',     }, ]   # Internationalization # https://docs.djangoproject.com/en/6.0/topics/i18n/  LANGUAGE_CODE = 'en-us'  TIME_ZONE = 'UTC'  USE_I18N = True  USE_TZ = True   # Static files (CSS, JavaScript, Images) # https://docs.djangoproject.com/en/6.0/howto/static-files/  STATIC_URL = '/static/' STATICFILES_DIRS = [BASE_DIR / 'static'] DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField' STATIC_ROOT = BASE_DIR / 'staticfiles'  CSRF_TRUSTED_ORIGINS = ['https://event-management-system-qr34.onrender.com']

--- DjangoProject1/settings.py
+++ DjangoProject1/settings.py
@@ -127,3 +127,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+CSRF_TRUSTED_ORIGINS = ['https://event-management-system-qr34.onrender.com']
